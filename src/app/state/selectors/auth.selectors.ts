c066be9dcb314ad662487ad04a557f0b4a855b4e
onebrother21 <service.onebrother@gmail.com>
2022-04-03 17:51:31 -0500

    rebuilding with minor changes

--- src/app/state/selectors/auth.selectors.ts
+++ src/app/state/selectors/auth.selectors.ts
@@ -5,5 +5,5 @@ export const auth$ = createFeatureSelector<AuthenticationState>("auth");
 export const authLoading$ = createSelector(auth$,s => s.loading);
 export const authErr$ = createSelector(auth$,s => s.error);
 export const authid$ = createSelector(auth$,s => s.username);
-export const authed$ = createSelector(auth$,s => !!(s.token && s.token.status));
+export const authed$ = createSelector(auth$,s => !!(s.token && s.token?.status||false));
 export const authExists$ = createSelector(auth$,s => s.exists);
